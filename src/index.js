9627d2919f8f15e945be6fd4167d053b18700bcc
Neill Robson <neill.robson@pendo.io>
2022-05-10 16:24:22 -0400

    Begin scaffolding blame function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-const install = (Vue) => {
+let Vue;
+
+/**
+ * Pass a reactive object or array into this function, and each time a change is
+ * triggered, the source of the reactive change will be printed to the browser
+ * console.
+ */
+const blame = () => {
+    /*
+     * We will create a dummy watcher on the reactive object passed in.  The
+     * watcher will have an array of dependencies, and we want to somehow
+     * retrieve the name of the dependency that triggered the re-computation.
+     */
+};
+
+const install = (_Vue) => {
     Vue.theNameOfNeill = 'Neill Robson';
+    Vue = _Vue;
+    Vue.blame = blame;
+    if (window) {
+        window.VueBlame = blame;
+    }
 };
 
 export default {
